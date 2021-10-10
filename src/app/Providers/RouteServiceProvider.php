a014d101a53c575e8e481115c6decb5bd6b7281a
Assane Sarr <contact@chicorycom.net>
2021-10-10 11:29:16 +0000

    update version framework

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -58,7 +58,6 @@ class RouteServiceProvider extends ServiceProvider
     {
 
         RateLimiter::for('api', function (Request $request) {
-
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
